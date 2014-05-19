c3d6f5907e06b0fede30216e8c32265bfa2d5c0a
91roseri <erik_ovrigt@hotmail.com>
2014-05-19 12:19:03 +0200

    textfält + mer

--- script/main.js
+++ script/main.js
@@ -12,11 +12,32 @@ function initialize() {
 	
 
 }
+function checkAlias(){
+	var helper = new CBHelper("benchpress", "37ff338f77e39490bad736e64bdd5839", new GenericHelper());
+	helper.setPassword(hex_md5("mopub_14"));
+if ($("#alias").val().length != 0){
+	
+	
+	helper.searchDocuments(
+		{"alias":$("#alias").val()}, "people", function(resp){
+			
+			
+			if(resp.callStatus && resp.outputData.length==0){
+				addPerson($("#alias").val());
+			}
+			else{
+				alert("Alias is already taken")
+			}
+		
+		});
+}else{
+	alert("You have to pick an alias!");
+}	
+}
 
 
 
-
-function addPerson(){
+function addPerson(alias){
 
   if (navigator.geolocation){
   	//console.log(navigator.geolocation);
@@ -36,7 +57,8 @@ function addPerson(){
 		
 		var dataObject = {
 			"lat_coords" : position.coords.latitude,
-			"lng_coords" : position.coords.longitude
+			"lng_coords" : position.coords.longitude,
+			"alias" : alias
 		};
 		
 		search(dataObject);
@@ -61,37 +83,54 @@ function addPerson(){
 				  return d; // returns the distance in meter
 				};
 				
+				console.log("ska lägga till");
 				helper.searchDocuments(
 					null, "people", function(resp){
 						var k=0; // En variabel som ökar vid varje "distanserad bänk"
 						for (var i=0; i<resp.outputData.length; i++){
-								var person = new google.maps.LatLng(resp.outputData[i].lat_coords, resp.outputData[i].lng_coords);
+							if(resp.outputData[i].length==0){
+								k=k+1;
+								console.log("tom")
+							}else{
+								var person = new google.maps.LatLng(resp.outputData[k].lat_coords, resp.outputData[k].lng_coords);
 								
 								if (getDistance(user,person)>30){ //Ska vi köra tio meter?
 									k=k+1;
 								}
+							}
 						}
 						if(k == resp.outputData.length){
 							add(dataObject);
 						}
 						else{
-							alert("Du står för nära någon annan, FLYTTA PÅ DIG!")
+							alert("You are too close to another player, MOVE!")
 						}
 					});
 				
 				};
 		function add(dataObject){
 			helper.insertDocument("people", dataObject, null, function(resp) {
-				alert("Du är nu gömd!");
+				document.location.href="#page-hidden";
 			});
 			};  
 	}
 };
-
+function deletePerson() {
+	var helper = new CBHelper("benchpress", "37ff338f77e39490bad736e64bdd5839", new GenericHelper());
+	helper.setPassword(hex_md5("mopub_14"));
+	console.log("ska ta bort ....");
+	new_object={};
+	helper.updateDocument(new_object, {"alias":$("#alias").val()}, "people",null, function(resp){
+	
+			alert("Removed");
+		});
+}
 function resize(dist) {
 	var size=(100/(1+dist*0.1));
 	console.log(size);
-	
+	if (size < 10){
+		size = 10;
+	}
 	$("#arrow").css({ 
 		'width' : size+'%',
 		'height' : size+'%'
@@ -144,12 +183,18 @@ function getDistance(){
 		
 		helper.searchDocuments(
 			null, "people", function(resp){
+				var k=0;
 			for (var i = 0; i < resp.outputData.length; i++){
+				if(resp.outputData[i].length==0){
+					console.log("tom")
+				}else{
 				var person_LatLng = new google.maps.LatLng(resp.outputData[i].lat_coords, resp.outputData[i].lng_coords);
 				var dist = getDistanceBetween(my_LatLng, person_LatLng);
-				window.peopleList[i]={"distance":dist, "lat_coords":resp.outputData[i].lat_coords,"lng_coords":resp.outputData[i].lng_coords, "my_LatLng":my_LatLng, "person_LatLng":person_LatLng};				
+				window.peopleList[k]={"distance":dist, "lat_coords":resp.outputData[i].lat_coords,"lng_coords":resp.outputData[i].lng_coords, "my_LatLng":my_LatLng, "person_LatLng":person_LatLng};				
+				k=k+1;//En list int
+				}
 			}			
-				$("#distance").html(window.peopleList[window.personNum].distance.toFixed(2));
+				//$("#distance").html(window.peopleList[window.personNum].distance.toFixed(2));
 				resize(window.peopleList[window.personNum].distance.toFixed(2));
 			
 			}
